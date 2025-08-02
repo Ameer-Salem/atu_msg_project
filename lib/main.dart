13d3dedfadd96b12aacc644f350d4a3b252bc909
ameer <ameer@email>
2025-08-02 10:24:01 +0300

    complete structure with foldering, add widgets and activate navigation destination

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     TextTheme textTheme = createTextTheme(context, 'Fredoka', "Fredoka");
 
     return AdaptiveTheme(
